f92e7539e7d4be056d43cb815cf63a3072602076
Sourav1343 <souravgupta1343@gmail.com>
2024-09-15 17:42:19 +0530

    Add User Entity with edge cases checks

--- src/main/java/com/portal/attendance/Entity/AttendanceEntity.java
+++
@@ -1,4 +0,0 @@
-package com.portal.attendance.Entity;
-
-public class AttendanceEntity {
-}
---
+++ src/main/java/com/portal/attendance/Entity/User.java
@@ -0,0 +1,36 @@
+package com.portal.attendance.Entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @NotBlank(message = "User name is mandatory")
+    @Size(min = 3, max = 100, message = "User name must be between 3 and 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    @Size(max = 255, message = "Email should not exceed 255 characters")
+    private String email;
+
+    @Column(name = "punch_time", nullable = false)
+    @NotNull(message = "Punch time is mandatory")
+    @PastOrPresent(message = "Punch time cannot be in the future")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
